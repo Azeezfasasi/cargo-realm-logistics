20852d40813a9d8e8bd7bc064b0fcc31fd36c58b
azeezfasasi <fas.azeez@gmail.com>
2025-08-01 15:43:14 +0100

    cool

--- src/config/Api.jsx
+++ src/config/Api.jsx
@@ -1,2 +1,2 @@
-export const BACKEND_URL = 'https://cac-lightway-backend.onrender.com';
-export const API_BASE_URL = 'https://cac-lightway-backend.onrender.com/api';
+export const BACKEND_URL = 'https://cargo-realm-backend.onrender.com';
+export const API_BASE_URL = 'https://cargo-realm-backend.onrender.com/api';
