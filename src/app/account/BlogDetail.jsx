4f3d5a7a4bf5c5c303f3aa64ac419df2cc47e71b
azeezfasasi <fas.azeez@gmail.com>
2025-08-06 10:18:00 +0100

    updated

--- src/app/account/BlogDetail.jsx
+++ src/app/account/BlogDetail.jsx
@@ -4,6 +4,8 @@ import DashMenu from '../../assets/component/DashboardComponents.jsx/DashMenu'
 import Header from '../../assets/component/HomeComponents.jsx/Header';
 import Footer from '../../assets/component/HomeComponents.jsx/Footer';
 import BlogDetailMain from '../../assets/component/DashboardComponents.jsx/BlogDetailMain';
+import HeaderSection from '@/assets/component/HomeComponents.jsx/HeaderSection';
+import FooterSection from '@/assets/component/HomeComponents.jsx/FooterSection';
 
 function BlogDetail() {
   return (
@@ -11,9 +13,9 @@ function BlogDetail() {
     <Helmet>
         <title>Blog Details - Cargo Realm and Logistics</title>
     </Helmet>
-    <Header />
+    <HeaderSection />
     <BlogDetailMain />
-    <Footer />
+    <FooterSection />
     </>
   )
 }
